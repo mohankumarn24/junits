a60de07c9928f87ba16d676ac513236495500c34
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-05-05 21:16:51 +0530

    refactor

--- src/test/java/net/projectsync/junits/JunitsApplicationTests.java
+++ src/test/java/net/projectsync/junits/JunitsApplicationTests.java
@@ -3,6 +3,7 @@ package net.projectsync.junits;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// This class is not needed. This slows down executing unit test cases as it uses @SpringBootTest
 @SpringBootTest
 class JunitsApplicationTests {
 
